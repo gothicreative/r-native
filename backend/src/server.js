9151c480f793938321c0562bdc52b8e71517a44c
hafiz adem <huhuhudadada393@gmail.com>
2025-08-28 16:20:15 +0300

    update the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,22 @@ import { connectDB } from './config/db.js';
 
 
 const app = express();
-connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on port', ENV.PORT);
-});
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error starting server:", error);
+  }
+}
+startServer();
