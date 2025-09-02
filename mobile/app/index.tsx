7fcfb6054ebaf475389abf642cc046bbafa2569d
hafiz adem <huhuhudadada393@gmail.com>
2025-09-02 14:11:47 +0300

    login auth added

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,12 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
-export default function Index() {
-  return (
-      <View className="flex-1 items-center justify-center bg-green-400">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind worked by hafiz
-        hi there is this real !
-      </Text>
-    </View>
-  );
+const HomeScreen = ()=>{
+  
+    return (
+      <View>
+        <Text> HomeScreen </Text>
+      </View>
+    )
+  
 }
+export default HomeScreen
