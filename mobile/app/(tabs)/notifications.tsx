9a309a1ef84f59ff97cf20c73d0230567979d51b
hafiz adem <huhuhudadada393@gmail.com>
2025-09-03 09:06:21 +0300

    tabs and logout added

---
+++ mobile/app/(tabs)/notifications.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+
+const notificationsScreen = ()=>{
+  
+    return (
+      <View>
+        <Text> search </Text>
+      </View>
+    )
+  
+}
+export default notificationsScreen
