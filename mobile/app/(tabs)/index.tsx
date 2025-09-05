37d9c19bfa36f1491f9a9528e27c962c95c40f8d
hafiz adem <huhuhudadada393@gmail.com>
2025-09-05 14:51:44 +0300

    upload data from local storage in to database

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,22 +1,52 @@
-import { Text, View } from 'react-native'
+import { ScrollView,RefreshControl, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useUserSync } from '@/hooks/useUserSync'
+import { usePosts } from "@/hooks/usePosts";
 import SignOutButton from '@/component/signOutButton'
-
+import { Ionicons } from "@expo/vector-icons";
+import PostComposer from '@/component/PostComposer';
+import { useState } from 'react';
+// import PostsList from "@/components/PostsList";
 
 
 
 const HomeScreen = ()=>{
+    const [isRefetching, setIsRefetching] = useState(false);
+  const { refetch: refetchPosts } = usePosts();
+
+  const handlePullToRefresh = async () => {
+    setIsRefetching(true);
+
+    await refetchPosts();
+    setIsRefetching(false);
+  };
 
   useUserSync();
 
     return (
 
-      <SafeAreaView className='flex-1'>
-        <View>
-        <Text>HomeScreens</Text>
-        </View>
+       <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
+        <Text className="text-xl font-bold text-gray-900">Home</Text>
         <SignOutButton />
+      </View>
+          <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={{ paddingBottom: 80 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefetching}
+            onRefresh={handlePullToRefresh}
+            tintColor={"#1DA1F2"}
+          />
+        }
+      >
+        <PostComposer />
+        {/* <PostsList /> */}
+      </ScrollView>
+
       </SafeAreaView>
 
     )
